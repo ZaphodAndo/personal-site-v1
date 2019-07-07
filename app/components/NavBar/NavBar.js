d331fe69635f1a6dacd30761d95deeb8c5701ea1
Ethan Anderson <ethan.anderson00@gmail.com>
2019-07-07 21:14:11 +0100

    Added animations and hover effects as well as card component and index content

--- app/components/NavBar/NavBar.js
+++ app/components/NavBar/NavBar.js
@@ -2,11 +2,11 @@ class NavBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = 
                         "<div>\
-                            <a href='/app/index.html'>Home</a>\
-                            <a>About</a>\
+                            <a href='/app/index.html' class='home'>Home</a>\
+                            <a href='/app/index.html'>About</a>\
                             <input class='search-box' type='text' placeholder='Search'>\
-                            <a>Blog</a>\
-                            <a>Categories</a>\
+                            <a href='/app/index.html'>Blog</a>\
+                            <a href='/app/index.html'>Categories</a>\
                         </div>";
     }
 }
