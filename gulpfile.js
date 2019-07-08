65f5d759fc9b99b3c098e8ae5ae875035686cb5f
Ethan Anderson <ethan.anderson00@gmail.com>
2019-07-08 22:56:37 +0100

    Fixed card block and changed background

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var wait = require('gulp-wait');
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss')
+    .pipe(wait(200))
       .pipe(sass()) // Using gulp-sass
       .pipe(gulp.dest('app/css'))
 });
